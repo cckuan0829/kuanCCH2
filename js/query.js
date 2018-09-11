c9a8b86c864eb60efbd5b9bc3d79ab6d593f7edd
jacky <ycchiu1024@gmail.com>
2018-09-11 23:27:42 +0800

    http get request demo

---
+++ js/query.js
@@ -0,0 +1,17 @@
+var url = 'http://api.chessdb.cn:81/chessdb.php?action=queryall&board=rnbakabnr/9/1c5c1/p1p1p1p1p/9/9/P1P1P1P1P/1C5C1/9/RNBAKABNR%20w';
+var data = httpGet(url);
+var queryBtn = document.getElementById("queryBtn");
+queryBtn.addEventListener("click", showResult);
+
+
+function showResult(){
+    document.getElementById("demo").innerHTML = data;
+}
+
+function httpGet(theUrl)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+    xmlHttp.send( null );
+    return xmlHttp.responseText;
+}
