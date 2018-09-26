3ea9fb4f391ccdce05700f77f085fd1cee309610
jacky <ycchiu1024@gmail.com>
2018-09-26 23:01:02 +0800

    added chart and library js file

---
+++ js/chart/chartDrawer.js
@@ -0,0 +1,47 @@
+var dataAry = [                
+    { x: 1, y: 0 },
+    { x: 2, y: 400 },
+    { x: 3, y: 100 },
+    { x: 4, y: 60 },
+    { x: 5, y: -120 },
+    { x: 6, y: 35 },
+    { x: 7, y: 400 },
+    { x: 8, y: -100 },
+    { x: 9, y: 60 },
+    { x: 10, y: 120 },
+];
+
+window.onload = function() {
+    $("#drawChartBtn").bind("click", function() {
+        drawChart();
+    });
+}
+
+function drawChart() {
+    //console.log(query_red_score);
+    var chart = new CanvasJS.Chart("chartContainer", {
+        animationEnabled: true,
+        title: {
+            text: "盤面分數走勢"
+        },
+        axisX: {
+            title: "回合"
+        },
+        axisY: {
+            title: "分數",
+            valueFormatString:"#"
+        // suffix: "points"
+        },
+        data: [{
+            type: "line",
+            name: "盤面絕對分數圖",
+            connectNullData: false,
+            //nullDataLineDashType: "solid",
+            xValueType: "int",
+          //  xValueFormatString: "DD MMM hh:mm TT",
+          //  yValueFormatString: "#,##0.##\"%\"",
+            dataPoints: dataAry
+        }]
+    });
+    chart.render();
+}
