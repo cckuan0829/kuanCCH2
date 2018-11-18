703d0e61774e8baa7f2dbedbaee45a3a42c1add6
Ching-Chun Kuan <domoanddomo@gmail.com>
2018-11-18 21:37:59 +0800

    add chessBoard.js

---
+++ js/board/chessBoard.js
@@ -0,0 +1,91 @@
+var pos = {};
+
+var chessType = {
+	'K':'帥',
+	'k':'將',
+	'A':'仕',
+	'a':'士',
+	'B':'相',
+	'b':'象',
+	'R':'車',
+	'r':'車',
+	'N':'馬',
+	'n':'馬',
+	'C':'炮',
+	'c':'炮',
+	'P':'兵',
+	'p':'卒',
+};
+
+var config = {
+	preChessPiece: 20,
+	preTd: 25,
+};
+
+var ChessPiece = function(x ,y, chess) {
+	this.x = x;
+	this.y = y;
+	this.chess = chess;
+	this.placement();
+};
+//把棋子放置在页面中
+ChessPiece.prototype.placement = function() {
+	if(this.chess == this.chess.toUpperCase()) this.DOM = $('<i class="chesspiece red">');
+	else this.DOM = $('<i class="chesspiece black">');
+	
+	this.DOM.html(chessType[this.chess])
+	this.DOM.css({
+		left: this.x*config.preTd - config.preChessPiece/2,
+		top: this.y*config.preTd - config.preChessPiece/2
+	});
+	pos[this.x+'-'+this.y] = this.DOM;   //通知pos
+	$list.append(this.DOM);
+};
+
+var ChessBoard = function(chessList) {
+	this.chessList = chessList;
+	this.initBoard();
+	this.placementAll();
+};
+
+ChessBoard.prototype.initBoard = function() {
+	$list = $('#chesslist');
+	$list.empty();
+}
+
+ChessBoard.prototype.placementAll = function() {
+	var ChessPieceList; 
+		
+	for(var i = 0; i < this.chessList.length; i++)
+	{
+		var chess_info = this.chessList[i];
+		
+		new ChessPiece(chess_info[0], chess_info[1], chess_info[2]);
+	}
+};
+
+function FEN_to_ChessList(fen)
+{
+    var chesslist=[];
+    var row_list = fen.split(/\/|%/);
+
+	for( var i = 0; i < 10; i++)
+	{
+        var index = 0
+        for ( var j = 0; j < row_list[i].length; j++)
+		{
+            var cha = row_list[i][j];
+            var val = cha.charCodeAt(0)-'0'.charCodeAt(0);
+            if( val <= 9 && val > 0)
+            {
+				index = index + val;
+            }
+			else
+			{
+                chesslist.push([index, i, cha]);
+                index = index + 1;
+			}
+		}
+	}
+    return chesslist;
+}
